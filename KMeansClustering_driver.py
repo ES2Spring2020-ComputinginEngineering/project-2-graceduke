1d5ed31a4257c279611ef3b1dafae10a2ff3faf3
graceduke <grace.duke@tufts.edu>
2020-04-15 20:43:26 -0400

    Final Commit

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -6,12 +6,13 @@ gnorm, hnorm, classification = kmc.normalizeData(glucose, hemoglobin, classifica
 
 k = 3
 
+
 centroids = kmc.centroid_points(k)
-c_assignment = kmc.assign_centroids (centroids, glucose, hemoglobin)
+c_assignment = kmc.assign_centroids (centroids, gnorm, hnorm)
 
-c_assignment, updated_array, glucose, hemoglobin, classification = kmc.iterate(2, 100)
+c_assignment, updated_array, gnorm, hnorm, classification = kmc.iterate(k, 100)
 
-kmc.graphKMeans(k, glucose, hemoglobin, c_assignment, updated_array)
+kmc.graphKMeans(k, gnorm, hnorm, c_assignment, updated_array)
 
-#TP, FP, TN, FN, CKD, nCKD = kmc.calc_positive_negatives(classification, c_assignment)
-#print (kmc.percentages(TP, FP, TN, FN, CKD, nCKD))
+TP, FP, TN, FN, CKD, nCKD = kmc.calc_positive_negatives(classification, c_assignment)
+print (kmc.percentages(TP, FP, TN, FN, CKD, nCKD))
